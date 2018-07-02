fd00fa3e8357418ad73a640ce7ec2ea46c112033
Jonathan RODRIGUES <jnt.rodrigues68@gmail.com>
2018-07-02 16:36:17 +0200

    material icon import + site footer 0.2

--- src/app/pages/site-footer/site-footer.component.ts
+++ src/app/pages/site-footer/site-footer.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-site-footer',
   templateUrl: './site-footer.component.html',
-  styleUrls: ['./site-footer.component.scss']
+  styleUrls: ['./site-footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SiteFooterComponent implements OnInit {
-
+  siteName = "e-Coach";
   constructor() { }
 
   ngOnInit() {
