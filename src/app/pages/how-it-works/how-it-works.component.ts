4ec57a9310a9122dcd6a8c02bf7277c10fcef6d4
Jonathan <jnt.rodrigues68@gmail.com>
2018-07-05 23:24:35 +0200

    ajout des url aux pages + security component 1.0 + comment-ca-marche 1.0

--- src/app/pages/how-it-works/how-it-works.component.ts
+++ src/app/pages/how-it-works/how-it-works.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-how-it-works',
   templateUrl: './how-it-works.component.html',
-  styleUrls: ['./how-it-works.component.scss']
+  styleUrls: ['./how-it-works.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HowItWorksComponent implements OnInit {
 
