4160c17196d9cf7fb0066befba6cdfb0bb1a501f
Jonathan RODRIGUES <jnt.rodrigues68@gmail.com>
2018-07-05 13:06:57 +0200

    rechargement gc 0.1 + home 0.9

--- src/app/pages/recharge/recharge.component.ts
+++ src/app/pages/recharge/recharge.component.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-recharge',
   templateUrl: './recharge.component.html',
-  styleUrls: ['./recharge.component.css']
+  styleUrls: ['./recharge.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RechargeComponent implements OnInit {
 
+  packs = [
+    {
+      'name' : 'XS',
+      'color': '#1faa00',
+      'gc': '10',
+      'price': '2'
+    },
+    {
+      'name' : 'SM',
+      'color': '#00bfa5',
+      'gc': '30',
+      'price': '5'
+    },
+    {
+      'name' : 'MD',
+      'color': '#2962ff',
+      'gc': '100',
+      'price': '15'
+    },
+    {
+      'name' : 'LG',
+      'color': '#aa00ff',
+      'gc': '200',
+      'price': '25'
+    },
+    {
+      'name' : 'XL',
+      'color': '#c51162',
+      'gc': '300',
+      'price': '30'
+    },
+    {
+      'name' : 'XXL',
+      'color': '#d50000',
+      'gc': '500',
+      'price': '50'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
