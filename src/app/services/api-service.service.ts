84690d216b7005355a2eb02884f0392567e6dd90
eXt-Ra <vanardois.romain@gmail.com>
2018-07-06 12:59:24 +0200

    api reserveCommande

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -54,4 +54,16 @@ export class ApiService {
       amount: amount,
     });
   }
+
+  reserveCommande(id, eventId, seanceSoloId, precision, mindgame, communication, deplacement, reflexe) {
+    return this.http.put(`${this.baseUrl}/commande/${id}`, {
+      eventId: eventId,
+      seanceSoloId: seanceSoloId,
+      precision: precision,
+      mindgame: mindgame,
+      communication: communication,
+      deplacement: deplacement,
+      reflexe: reflexe,
+    });
+  }
 }
